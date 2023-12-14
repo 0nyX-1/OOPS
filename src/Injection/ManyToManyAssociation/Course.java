d070435128e04492b2a992d505f0d8faaa9854ac
0nyX-1 <141195662+0nyX-1@users.noreply.github.com>
2023-12-14 16:34:25 +0545

    Inheritance

---
+++ src/Injection/ManyToManyAssociation/Course.java
@@ -0,0 +1,11 @@
+package Injection.ManyToManyAssociation;
+
+public class Course {
+    String courseName;
+    int Value;
+
+    public Course(String courseName, int value) {
+        this.courseName = courseName;
+        this.Value = value;
+    }
+}
